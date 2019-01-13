02978705de711d2bb458a2b00fe34cb553476848
Piotr <ptrkchn@gmail.com>
2019-01-13 10:54:13 +0100

    change gecko id

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -29,7 +29,7 @@ var manifest = {
   firefox: {
     "applications": {
       "gecko": {
-        "id": "my-app-id@mozilla.org"
+        "id": "allegro-poszukiwacz-niepromowanych@piotrkochan"
       }
     }
   }
